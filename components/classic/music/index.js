dcded91c9187c38ae8ef54269952a6c1cad2f4ef
yahnzt <yahpro@outlook.com>
2019-04-09 16:59:47 +0800

    add music play

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,6 +1,8 @@
 // components/classic/music/index.js
 import {classicBeh} from '../classic-beh.js'
 
+const mMgr = wx.getBackgroundAudioManager();
+  
 Component({
   /**
    * 组件的属性列表
@@ -8,20 +10,68 @@ Component({
   behaviors: [classicBeh],
 
   properties: {
+    src: String,
+    title: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    playing: false,
     pauseSrc: 'images/player@waitting.png',
-    playSrc: 'images/player@playing.png'
+    playSrc: 'images/player@playing.png',
+  },
+
+  attached: function(event) {
+    this._recoverStatus()
+    this._monitorSwitch()
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
+    onPlay:function (event) {
+      if (!this.data.playing) {
+        this.setData({
+          playing: true
+        })
+        mMgr.src = this.properties.src
+        mMgr.title = this.properties.title
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    },
+      _recoverStatus:function() {
+        if (mMgr.paused) {
+          this.setData({
+            playing: false
+          })
+          return
+        }
+        if (mMgr.src == this.properties.src) {
+          this.setData({
+            playing: true
+          })
+        }
+      },
+      _monitorSwitch: function() {
+        mMgr.onPlay(()=>{
+          this._recoverStatus()
+        })
+         mMgr.onPause(()=>{
+          this._recoverStatus()
+        })
+         mMgr.onStop(()=>{
+          this._recoverStatus()
+        })
+         mMgr.onEnded(()=>{
+          this._recoverStatus()
+        })
+      }
+    }
 })
